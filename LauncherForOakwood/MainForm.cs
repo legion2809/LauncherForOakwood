c6dedfd2ffab138de99ee8e2f54a97c4a53738d0
Yerkanat Sheriyazdanov <56153705+legion2809@users.noreply.github.com>
2021-08-24 23:25:18 +0600

    Added some exception handling

--- LauncherForOakwood/MainForm.cs
+++ LauncherForOakwood/MainForm.cs
@@ -16,61 +16,73 @@ namespace LauncherForOakwood
         #region Writing client.json and launcher.json
         private void WriteSettings()
         {
-            string path = OakPathBox.Text + "\\config\\launcher.json";
-            JsonLauncher launcher = new JsonLauncher()
+            try
             {
-                gamepath = GamepathBox.Text,
-                width = Convert.ToInt32(WidthBox.Text),
-                height = Convert.ToInt32(HeightBox.Text),
-                fullscreen = isFullScreen.Checked ? true : false,
-                antialiasing = Convert.ToInt32(antiAlias.Text)
-            };
+                string path = OakPathBox.Text + "\\config\\launcher.json";
+                JsonLauncher launcher = new JsonLauncher()
+                {
+                    gamepath = GamepathBox.Text,
+                    width = Convert.ToInt32(WidthBox.Text),
+                    height = Convert.ToInt32(HeightBox.Text),
+                    fullscreen = isFullScreen.Checked ? true : false,
+                    antialiasing = Convert.ToInt32(antiAlias.Text)
+                };
 
-            var settings = JsonConvert.SerializeObject(launcher, Formatting.Indented);
+                var settings = JsonConvert.SerializeObject(launcher, Formatting.Indented);
 
-            using (var writer = new StreamWriter(path))
+                using (var writer = new StreamWriter(path))
+                {
+                    writer.Write(settings);
+                }
+
+                string cpath = OakPathBox.Text + "\\config\\client.json";
+                JsonClient client = new JsonClient()
+                {
+                    temp_nickname = NicknameBox.Text
+                };
+
+                var nname = JsonConvert.SerializeObject(client, Formatting.Indented);
+
+                using (var writer = new StreamWriter(cpath))
+                {
+                    writer.Write(nname);
+                }
+
+                Process.Start(OakPathBox.Text + "\\Oakwood.exe");
+                Application.Exit();
+            } catch
             {
-                writer.Write(settings);
+                MessageBox.Show("Something went wrong!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            
-            string cpath = OakPathBox.Text + "\\config\\client.json";
-            JsonClient client = new JsonClient()
-            {
-                temp_nickname = NicknameBox.Text
-            };
-
-            var nname = JsonConvert.SerializeObject(client, Formatting.Indented);
-
-            using (var writer = new StreamWriter(cpath))
-            {
-                writer.Write(nname);
-            }
-
-            Process.Start(OakPathBox.Text + "\\Oakwood.exe");
-            Application.Exit();
         }
         #endregion
 
         #region Parsing JaSON files
         private void readJSONButton_Click(object sender, EventArgs e)
         {
-            DialogResult result = openJSON.ShowDialog();
-            dynamic filename = JsonConvert.DeserializeObject(File.ReadAllText(openJSON.FileName));
-
-            if (result == DialogResult.OK)
+            try
             {
-                if (openJSON.FileName.Contains("client.json"))
+                DialogResult result = openJSON.ShowDialog();
+                dynamic filename = JsonConvert.DeserializeObject(File.ReadAllText(openJSON.FileName));
+
+                if (result == DialogResult.OK)
                 {
-                    NicknameBox.Text = filename["temp_nickname"];
+                    if (openJSON.FileName.Contains("client.json"))
+                    {
+                        NicknameBox.Text = filename["temp_nickname"];
+                    }
+                    else if (openJSON.FileName.Contains("launcher.json"))
+                    {
+                        WidthBox.Text = filename["width"];
+                        HeightBox.Text = filename["height"];
+                        GamepathBox.Text = filename["gamepath"];
+                        isFullScreen.Checked = filename["fullscreen"] == false ? false : true;
+                        antiAlias.Text = filename["antialiasing"];
+                    }
                 }
-                else if (openJSON.FileName.Contains("launcher.json"))
-                {
-                    WidthBox.Text = filename["width"];
-                    HeightBox.Text = filename["height"];
-                    GamepathBox.Text = filename["gamepath"];
-                    isFullScreen.Checked = filename["fullscreen"] == false ? false : true;
-                    antiAlias.Text = filename["antialiasing"];
-                }
+            } catch
+            {
+                MessageBox.Show("Something went wrong!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
         #endregion
